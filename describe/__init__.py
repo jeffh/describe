0f812f97ee31cc7b86ca0563a2660d62d6a44e86
Jeff Hui <contrib@jeffhui.net>
2011-04-29 17:05:55 -0400

    Removed nose_plugin import from describe/__init__.py -- the plan is to eventually remove reliance on nose.

--- describe/__init__.py
+++ describe/__init__.py
@@ -2,7 +2,6 @@ from value import Value
 from mock import Mock, repository
 from mock import args_filter as arg
 from spec import Spec
-from nose_plugin import SpecPlugin
 
 # untested. Removed
 #from main import run_spec
